b36754fba77650b963a2103f36e3ba09bf9a8d48
ItLearningPfe <asmaa.bohout@itlearning-campus.org>
2025-07-26 21:29:36 +0200

    Add files via upload

--- UApp.py
+++ UApp.py
@@ -277,7 +277,7 @@ with tab1:
    
 
     # Affichage des KPI
-    col3, col4, col5, col6, col7, col8, col9 = st.columns([1,1,1,1,1,1,1])
+    col3, col4, col5 = st.columns([1,1,1])
 
     with col3:
         st.markdown(custom_kpi_card("Total impayés", f"{total_impayés_locataires_partis:,.2f}€".replace(",", " ").replace(".", ",").replace(" ", "."), "kpi-orange"), unsafe_allow_html=True)
